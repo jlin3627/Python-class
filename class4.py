18fa3c778bc4e7816c461dbcae65f839f9570b72
jlin3627 <54677175+jlin3627@users.noreply.github.com>
2023-05-31 20:24:14 -0400

    class 4 turtle basics

---
+++ class4.py
@@ -0,0 +1,19 @@
+from turtle import *
+#function
+#circle(200,360,1)
+#circle(200,180,1000)
+color("pink")
+begin_fill()
+for i in range(4):
+    forward(90)
+    right(100)
+end_fill()
+done()
+#forward(200)
+#circle(20,90)
+#forward(200)
+#circle(20,180)
+#forward(200)
+#circle(20,270)
+#forward(200)
+#circle(20,360)
